33800c2dd74fe64dbc67d25967044201514d9ba5
zhuyk6 <zhuyk6@foxmail.com>
2024-12-17 23:30:31 +0800

    🐎 ci(docs): add docs ci

--- tests/test_fib.py
+++ tests/test_fib.py
@@ -1,11 +1,21 @@
+from mypkg.fib import fib
 import pytest
-import mypkg.fib as fib
 
 
 def test_fib():
-    assert fib.fib(0) == 1
-    assert fib.fib(1) == 1
-    assert fib.fib(5) == 8
+    assert fib(0) == 1
+    assert fib(1) == 1
+    assert fib(5) == 8
 
+
+def test_fib_exception():
     with pytest.raises(ValueError):
-        fib.fib(-1)
+        fib(-1)
+
+
+def test_github_actions_ruff_lint():
+    pass
+
+
+def test_github_actions_ruff_format():
+    fib(1)
